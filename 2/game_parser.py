a7845acf7754be7cba4d6aacd482cddc7bc72dbd
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-03 12:35:53 +0100

    day 2 exercise 1, game parser

---
+++ 2/game_parser.py
@@ -0,0 +1,28 @@
+from functools import reduce
+
+def parse_game(game):
+    [gameNum, rounds] = game.split(":")
+    parsed_rounds = map(lambda round: parse_round(round), rounds.split(";"))
+    parsed_rounds = reduce(lambda r1, r2: merge_rounds(r1, r2), parsed_rounds)
+    return parsed_rounds
+
+
+def parse_round(round):
+    dice = round.split(",")
+    parsed_dice = map(lambda die: parse_die_color(die), dice)
+    return reduce((lambda d1, d2: d1 | d2), parsed_dice)
+
+
+def parse_die_color(die_color):
+    [amount, color] = die_color.strip().split()
+    return {
+        color: int(amount)
+    }
+
+def merge_rounds(round1, round2):
+    keys = set(list(round1.keys()) + list(round2.keys()))
+    merged = {}
+    for k in keys:
+        merged[k] = max(round1.get(k, 0), round2.get(k, 0))
+
+    return merged
---
+++ 2/test_parser.py
@@ -0,0 +1,38 @@
+from game_parser import *
+
+def test_parse_die_color():
+    color = "3 blue"
+    parsed = parse_die_color(color)
+    assert parsed == {
+        "blue": 3
+    }
+
+def test_parse_round():
+    color = "3 blue, 4 red"
+    parsed = parse_round(color)
+    assert parsed == {
+        "blue": 3,
+        "red": 4
+    }
+
+def test_merge_rounds():
+    r1 = {
+        "blue": 3,
+        "red": 4
+    }
+    r2 = {
+        "blue": 5,
+        "green": 4
+    }
+
+    merged = merge_rounds(r1, r2)
+    assert merged["blue"] == 5
+    assert merged["green"] == 4
+    assert merged["red"] == 4
+
+def test_parse_game():
+    game = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    parsed = parse_game(game)
+    assert parsed["blue"] == 6
+    assert parsed["red"] == 4
+    assert parsed["green"] == 2
