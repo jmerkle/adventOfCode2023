58127c3d9dba06fefe5d3af8406e408eebf452c0
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-04 18:58:20 +0100

    day 3 exercise 2 find and return positions of gears for coord

--- 3/functions.py
+++ 3/functions.py
@@ -43,3 +43,35 @@ def is_symbol(char):
 def find_numbers_with_position_indexes(line):
     res = [[int(match.group()), match.start(), match.end()] for match in re.finditer(r'\d+', line)]
     return res
+
+# def find_adjacent_gear_for_found_number(data, line, found_number):
+
+def find_adjacent_gear_for_position(matrix, line, column):
+    findings = []
+
+    max_line = len(matrix) - 1
+    max_column = len(matrix[0]) - 1
+    # above left
+    if line > 0 and column > 0 and is_gear(matrix[line - 1][column - 1]): findings.append(to_coord_string(line - 1, column - 1))
+    # above
+    if line > 0 and is_gear(matrix[line - 1][column]): findings.append(to_coord_string(line - 1, column))
+    # above right
+    if line > 0 and column < max_column and is_gear(matrix[line - 1][column + 1]): findings.append(to_coord_string(line - 1, column + 1))
+    # left
+    if column > 0 and is_gear(matrix[line][column - 1]): findings.append(to_coord_string(line, column - 1))
+    # right
+    if column < max_column and is_gear(matrix[line][column + 1]): findings.append(to_coord_string(line, column + 1))
+    # below_left
+    if line < max_line and column > 0 and is_gear(matrix[line + 1][column - 1]): findings.append(to_coord_string(line + 1, column - 1))
+    # below
+    if line < max_line and is_gear(matrix[line + 1][column]): findings.append(to_coord_string(line + 1, column))
+    # below right
+    if line < max_line and column < max_column and is_gear(matrix[line + 1][column + 1]): findings.append(to_coord_string(line + 1, column + 1))
+
+    return findings
+
+def is_gear(char):
+    return char == "*"
+
+def to_coord_string(line, column):
+    return str(line) + "-" + str(column)
--- 3/test_process_line.py
+++ 3/test_process_line.py
@@ -1,4 +1,9 @@
 from functions import *
+
+data_small = """467..114..
+...*......
+..35..633."""
+
 def test_find_surrounding_symbols():
     data = """467..114..
             ...*......
@@ -40,3 +45,30 @@ def test_find_position_of_number():
     assert result[0] == [467, 0, 3]
     assert result[1] == [114, 5, 8]
     assert len(result) == 2
+
+# def test_find_adjacent_gears_for_found_number():
+#     data = """467..114..
+# ...*......
+# ..35..633."""
+#
+#     found_number = [467, 0, 3]
+#     line = 0
+#
+#     result = find_adjacent_gear_for_found_number(data, line, found_number)
+#
+#     assert result == ["1-3", 467]
+
+def test_find_adjacent_gear_for_position():
+    matrix = data_small.split("\n")
+
+    result = find_adjacent_gear_for_position(matrix, 0, 2)
+    assert result == ["1-3"]
+
+
+def test_find_multiple_adjacent_gears_for_position():
+    matrix = """467*.114..
+...*......
+..35..633.""" .split("\n")
+
+    result = find_adjacent_gear_for_position(matrix, 0, 2)
+    assert result == ["0-3", "1-3"]
