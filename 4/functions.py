127ed5f0ba83b11e1c50f2bac9b838883e72be4f
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-04 21:20:23 +0100

    day 4 exercise 1

---
+++ 4/functions.py
@@ -0,0 +1,24 @@
+import re
+def read_file_as_list_of_lines_and_filter_empty_lines(filename):
+    f = open(filename, 'r')
+    data = f.read()
+    f.close()
+    return [x for x in data.split("\n") if len(x) > 0]
+
+def exercise_1(data):
+    points = [calculate_points_for_card(card) for card in data]
+    return sum(points)
+
+def calculate_points_for_card(card):
+    [winning_numbers, card_numbers] = parse_card(card)
+    numbers_in_both_lists = set(winning_numbers) & set(card_numbers)
+    if len(numbers_in_both_lists) < 1:
+        return 0
+    return 2 ** (len(numbers_in_both_lists)-1)
+
+def parse_card(card):
+    [winning_string, card_string] = card.split(":")[1].split("|")
+    return [extract_numbers(winning_string), extract_numbers(card_string)]
+
+def extract_numbers(text):
+    return [int(n) for n in re.findall(r'\d+', text)]
---
+++ 4/test_day_4.py
@@ -0,0 +1,25 @@
+from functions import *
+
+data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt')
+def test_read_file():
+    data = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt')
+    assert data[0] == "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+    assert data[-1] == "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+def test_parse_card():
+    card = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+    [winning_numbers, card_numbers] = parse_card(card)
+    assert winning_numbers == [41, 48, 83, 86, 17]
+    assert card_numbers == [83, 86, 6, 31, 17, 9, 48, 53]
+def test_calculate_points_for_card():
+    card = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+    assert calculate_points_for_card(card) == 8
+
+def test_calculate_points_for_losing_card():
+    card = "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"
+    assert calculate_points_for_card(card) == 0
+def test_exercise_1():
+    assert exercise_1(data_small) == 13
+
+def test_exercise_1_full_data():
+    data = read_file_as_list_of_lines_and_filter_empty_lines('input.txt')
+    print(exercise_1(data))
