fb56c2207ee1c17ac795fbc267947c88b7f90258
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-05 12:08:45 +0100

    day 5 exercise 1 parse mappings and retrieve value from mapping

--- 5/functions_day_5.py
+++ 5/functions_day_5.py
@@ -1,8 +1,32 @@
+import re
+
+
 def read_file_as_list_of_lines_and_filter_empty_lines(filename):
     f = open(filename, 'r')
     data = f.read()
     f.close()
-    return [x for x in data.split("\n") if len(x) > 0]
+    return [x.strip() for x in data.split("\n\n") if len(x) > 0]
+
+
+def parse_mapping(mapping_as_text):
+    lines = mapping_as_text.split("\n")
+    if not lines[0].endswith("map:"):
+        raise Exception("invalid mapping format: " + mapping_as_text)
+    return [extract_numbers(line) for line in lines[1:]]
+
+
+def extract_numbers(text):
+    return [int(n) for n in re.findall(r'\d+', text)]
+
+
+def find_value_in_mapping(mapping, value):
+    matching_row = [row for row in mapping if row[1] <= value < row[1] + row[2]]
+    if len(matching_row) > 1:
+        raise Exception("more than one row found in mapping" + str(matching_row))
+    if len(matching_row) == 0:
+        return value
+    matching_row = matching_row[0]
+    return matching_row[0] + value - matching_row[1]
 
 
 def exercise_1(data):
--- 5/test_day_5.py
+++ 5/test_day_5.py
@@ -3,5 +3,36 @@ from functions_day_5 import *
 data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt')
 
 
+def test_read_file():
+    sections = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt')
+    assert len(sections) == 8
+    assert sections[0] == "seeds: 79 14 55 13"
+    assert sections[1] == """seed-to-soil map:
+50 98 2
+52 50 48"""
+    assert sections[-1] == """humidity-to-location map:
+60 56 37
+56 93 4"""
+
+
+def test_parse_mapping():
+    mapping_as_text = """seed-to-soil map:
+50 98 2
+52 50 48"""
+
+    parsed = parse_mapping(mapping_as_text)
+
+    assert parsed == [[50, 98, 2], [52, 50, 48]]
+
+
+def test_find_value_in_mapping():
+    mapping = [[50, 98, 2], [52, 50, 48]]
+
+    assert find_value_in_mapping(mapping, 79) == 81
+    assert find_value_in_mapping(mapping, 14) == 14
+    assert find_value_in_mapping(mapping, 55) == 57
+    assert find_value_in_mapping(mapping, 13) == 13
+
+
 def test_exercise_1():
     assert exercise_1(data_small) == 35
