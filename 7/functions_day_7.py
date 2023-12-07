e1c49f5482a1e6aa4741157f28881a653389157c
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-07 22:59:39 +0100

    day 7 exercise 1 done. With types!1

--- 7/functions_day_7.py
+++ 7/functions_day_7.py
@@ -1,10 +1,60 @@
+from collections import Counter
 
-def read_file_as_list_of_lines_and_filter_empty_lines(filename):
+
+def read_file_as_list_of_lines_and_filter_empty_lines(filename: str):
     f = open(filename, 'r')
     data = f.read()
     f.close()
     return [x for x in data.split("\n") if len(x) > 0]
 
 
-def exercise_1(data):
+def hand_type(cards: str) -> int:
+    card_frequencies = sorted(Counter(cards).values(), reverse=True)
+    if card_frequencies[0] == 5:
+        return 6
+    if card_frequencies[0] == 4:
+        return 5
+    if card_frequencies[0] == 3 and card_frequencies[1] == 2:
+        return 4
+    if card_frequencies[0] == 3:
+        return 3
+    if card_frequencies[0] == 2 and card_frequencies[1] == 2:
+        return 2
+    if card_frequencies[0] == 2:
+        return 1
     return 0
+
+
+def with_sortable_chars(card: str) -> str:
+    return (card
+            .replace("A", "E")
+            .replace("K", "D")
+            .replace("Q", "C")
+            .replace("J", "B")
+            .replace("T", "A")
+            )
+
+
+def enumerate_hand(hand: str) -> str:
+    [cards, _] = hand.split()
+    return str(hand_type(cards)) + with_sortable_chars(cards)
+
+
+def rank_hands(data: list[str]) -> list[str]:
+    return sorted(data, key=enumerate_hand)
+
+
+def apply_bid(hand_idx: tuple[int, str]) -> int:
+    index, hand = hand_idx
+    [_, bid] = hand.split()
+    return (index + 1) * int(bid)
+
+
+def apply_bids(hands_ranked: list[str]) -> list[int]:
+    return list(map(apply_bid, enumerate(hands_ranked)))
+
+
+def exercise_1(data: list[str]) -> int:
+    hands_ranked = rank_hands(data)
+    winnings = apply_bids(hands_ranked)
+    return sum(winnings)
--- 7/test_day_7.py
+++ 7/test_day_7.py
@@ -1,8 +1,38 @@
 from functions_day_7 import *
 
 data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt')
+data_full = read_file_as_list_of_lines_and_filter_empty_lines('input.txt')
+
+
+def test_hand_type():
+    assert hand_type("23456") == 0
+    assert hand_type("A23A4") == 1
+    assert hand_type("23432") == 2
+    assert hand_type("TTT98") == 3
+    assert hand_type("23332") == 4
+    assert hand_type("AA8AA") == 5
+    assert hand_type("AAAAA") == 6
+
+
+def test_enumerate_hand():
+    assert enumerate_hand("32T3K 765") < enumerate_hand("KTJJT 220")
+    assert enumerate_hand("KTJJT 220") < enumerate_hand("KK677 28")
+    assert enumerate_hand("KK677 28") < enumerate_hand("T55J5 684")
+    assert enumerate_hand("T55J5 684") < enumerate_hand("QQQJA 483")
+
+
+def test_rank_hands():
+    hands_ranked = rank_hands(data_small)
+    assert hands_ranked[0] == "32T3K 765"
+    assert hands_ranked[1] == "KTJJT 220"
+    assert hands_ranked[2] == "KK677 28"
+    assert hands_ranked[3] == "T55J5 684"
+    assert hands_ranked[4] == "QQQJA 483"
 
 
 def test_exercise_1():
     assert exercise_1(data_small) == 6440
 
+
+def test_exercise_1_all_data():
+    print(exercise_1(data_full))
