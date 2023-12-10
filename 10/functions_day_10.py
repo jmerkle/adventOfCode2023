6fb02abf3a9790f6304e118175c143e3fdba0599
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-10 22:32:35 +0100

    day 10 exercise 2 done

--- 10/functions_day_10.py
+++ 10/functions_day_10.py
@@ -1,11 +1,11 @@
 import math
 
 
-def read_file_as_list_of_lines_and_filter_empty_lines(filename: str):
+def read_file_as_list_of_list_of_chars_and_filter_empty_lines(filename: str):
     f = open(filename, 'r')
     data = f.read()
     f.close()
-    return [x for x in data.split("\n") if len(x) > 0]
+    return [list(x) for x in data.split("\n") if len(x) > 0]
 
 
 def calc_position(position_line: int, position_column: int, direction: str) -> tuple[int, int]:
@@ -46,19 +46,96 @@ def calc_direction(direction: str, pipe_shape: str) -> str:
     raise Exception("cannot go to ", pipe_shape, " with ", direction)
 
 
-def take_step(data: list[str], position_line: int, position_column: int, direction: str) -> tuple[int, int, str]:
+def mark_areas(areas: list[list[str]], position_line: int, position_column: int, direction: str, pipe_shape: str) -> list[list[str]]:
+    if pipe_shape == "|" and direction == "D":
+        areas[position_line][position_column-1] = "1"
+        areas[position_line][position_column+1] = "0"
+    if pipe_shape == "|" and direction == "U":
+        areas[position_line][position_column-1] = "0"
+        areas[position_line][position_column+1] = "1"
+    if pipe_shape == "-" and direction == "R":
+        areas[position_line-1][position_column] = "0"
+        areas[position_line+1][position_column] = "1"
+    if pipe_shape == "-" and direction == "L":
+        areas[position_line-1][position_column] = "1"
+        areas[position_line+1][position_column] = "0"
+    if pipe_shape == "L" and direction == "D":
+        areas[position_line+1][position_column] = "1"
+        areas[position_line][position_column-1] = "1"
+    if pipe_shape == "L" and direction == "L":
+        areas[position_line+1][position_column] = "0"
+        areas[position_line][position_column-1] = "0"
+    if pipe_shape == "J" and direction == "D":
+        areas[position_line+1][position_column] = "0"
+        areas[position_line][position_column+1] = "0"
+    if pipe_shape == "J" and direction == "R":
+        areas[position_line+1][position_column] = "1"
+        areas[position_line][position_column+1] = "1"
+    if pipe_shape == "7" and direction == "U":
+        areas[position_line-1][position_column] = "1"
+        areas[position_line][position_column+1] = "1"
+    if pipe_shape == "7" and direction == "R":
+        areas[position_line-1][position_column] = "0"
+        areas[position_line][position_column+1] = "0"
+    if pipe_shape == "F" and direction == "U":
+        areas[position_line-1][position_column] = "0"
+        areas[position_line][position_column-1] = "0"
+    if pipe_shape == "F" and direction == "L":
+        areas[position_line-1][position_column] = "1"
+        areas[position_line][position_column-1] = "1"
+    return areas
+
+
+def take_step(data: list[list[str]], areas: list[list[str]], position_line: int, position_column: int, direction: str) -> tuple[list[list[str]], int, int, str]:
     new_line, new_column = calc_position(position_line, position_column, direction)
-    new_tile = data[new_line][new_column]
-    new_direction = calc_direction(direction, new_tile)
-    return new_line, new_column, new_direction
+    new_tile_pipe_shape = data[new_line][new_column]
+    new_direction = calc_direction(direction, new_tile_pipe_shape)
+    if len(areas) > 0:
+        areas = mark_areas(areas, new_line, new_column, direction, new_tile_pipe_shape)
+    return areas, new_line, new_column, new_direction
 
 
-def exercise_1(data: list[str], start_line: int, start_column: int, start_direction: str) -> int:
+def exercise_1(data: list[list[str]], start_line: int, start_column: int, start_direction: str) -> int:
     steps_taken = 0
     position_line = start_line
     position_column = start_column
     direction = start_direction
     while direction != "S":
-        position_line, position_column, direction = take_step(data, position_line, position_column, direction)
+        _, position_line, position_column, direction = take_step(data, [[]], position_line, position_column, direction)
         steps_taken += 1
     return math.ceil(steps_taken/2)
+
+
+def matrix_to_string(data: list[list[str]]) -> str:
+    return "\n".join(["".join(line) for line in data])
+
+
+def fill_with_dots(output: str) -> str:
+    chars_to_replace = ["|", "-", "L", "J", "7", "F"]
+    for c in chars_to_replace:
+        output = output.replace(c, ".")
+    return output
+
+
+def exercise_2(data: list[list[str]], start_line: int, start_column: int, start_direction: str, start_pipe_shape: str, start_incoming_direction: str)-> int:
+    areas = [["."]*len(data[0]) for i in range(len(data))]
+    areas = mark_areas(areas, start_line, start_column, start_incoming_direction, start_pipe_shape)
+    steps_taken = 0
+    position_line = start_line
+    position_column = start_column
+    direction = start_direction
+    while direction != "S":
+        areas, position_line, position_column, direction = take_step(data, areas, position_line, position_column, direction)
+        data[position_line][position_column] = "*"
+        steps_taken += 1
+    visited_tiles = fill_with_dots(matrix_to_string(data))
+    areas_as_string = matrix_to_string(areas)
+    merged_maps = ""
+    for i in range(len(visited_tiles)):
+        if visited_tiles[i] == "*":
+            merged_maps += "*"
+        else:
+            merged_maps += areas_as_string[i]
+    while merged_maps.find("1.") > 0:
+        merged_maps = merged_maps.replace("1.", "11")
+    return sum([c == "1" for c in merged_maps])
--- 10/test_day_10.py
+++ 10/test_day_10.py
@@ -1,8 +1,9 @@
 from functions_day_10 import *
 
-data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt')
-data_small_more_complex = read_file_as_list_of_lines_and_filter_empty_lines('input_small_more_complex.txt')
-data_full = read_file_as_list_of_lines_and_filter_empty_lines('input.txt')
+data_small = read_file_as_list_of_list_of_chars_and_filter_empty_lines('input_small.txt')
+data_small_more_complex = read_file_as_list_of_list_of_chars_and_filter_empty_lines('input_small_more_complex.txt')
+data_small_exercise_2 = read_file_as_list_of_list_of_chars_and_filter_empty_lines('input_exercise_2_test.txt')
+data_full = read_file_as_list_of_list_of_chars_and_filter_empty_lines('input.txt')
 
 
 def test_exercise_1():
@@ -15,3 +16,12 @@ def test_exercise_1():
 def test_exercise_1_full():
     assert (exercise_1(data_full, 62, 61, "D")) == 6815
     assert (exercise_1(data_full, 62, 61, "L")) == 6815
+
+
+def test_exercise_2():
+    assert exercise_2(data_small_exercise_2, 1, 1, "R", "F", "U") == 4
+
+
+def test_exercise_2_full():
+    assert exercise_2(data_full, 62, 61, "D", "7", "R") == 269
+
