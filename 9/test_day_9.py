d11b38091a5a3747bb8c8d4d18b60a7e2d9ebe92
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-10 18:02:02 +0100

    day 9 exercise 1 setup

---
+++ 9/test_day_9.py
@@ -0,0 +1,9 @@
+from functions_day_9 import *
+
+data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt')
+data_full = read_file_as_list_of_lines_and_filter_empty_lines('input.txt')
+
+
+def test_exercise_1():
+    assert exercise_1(data_small) == 114
+
