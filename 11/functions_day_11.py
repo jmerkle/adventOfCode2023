b38b25b85f7c4741bc533c3160237ff0124ee204
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-11 10:23:30 +0100

    day 11 exercise 1 find expanding rows and columns

--- 11/functions_day_11.py
+++ 11/functions_day_11.py
@@ -1,12 +1,30 @@
-from collections import Counter
+import numpy as np
 
 
-def read_file_as_list_of_lines_and_filter_empty_lines(filename: str):
+def read_file_as_array_and_filter_empty_lines(filename: str) -> np.ndarray:
     f = open(filename, 'r')
     data = f.read()
     f.close()
-    return [x for x in data.split("\n") if len(x) > 0]
+    return np.array([list(x) for x in data.split("\n") if len(x) > 0])
 
 
-def exercise_1(data: list[str]) -> int:
+def find_expanding_lines(data: np.ndarray) -> list[int]:
+    expanding_lines = []
+    for line in range(0, data.shape[0]):
+        if all(c == "." for c in data[line, :]):
+            expanding_lines.append(line)
+    return expanding_lines
+
+
+def find_expanding_columns(data: np.ndarray) -> list[int]:
+    expanding_columns = []
+    for column in range(0, data.shape[1]):
+        if all(c == "." for c in data[:, column]):
+            expanding_columns.append(column)
+    return expanding_columns
+
+
+def exercise_1(data: np.ndarray) -> int:
+    expanding_line = find_expanding_lines(data)
+    expanding_columns = find_expanding_columns(data)
     return 0
--- 11/test_day_11.py
+++ 11/test_day_11.py
@@ -1,8 +1,16 @@
 from functions_day_11 import *
+import numpy as np
 
-data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt')
-data_full = read_file_as_list_of_lines_and_filter_empty_lines('input.txt')
+data_small = read_file_as_array_and_filter_empty_lines('input_small.txt')
+data_full = read_file_as_array_and_filter_empty_lines('input.txt')
 
+
+def test_find_expanding_lines():
+    assert find_expanding_lines(data_small) == [3, 7]
+
+
+def test_find_expanding_columns():
+    assert find_expanding_columns(data_small) == [2, 5, 8]
 
 
 def test_exercise_1():
