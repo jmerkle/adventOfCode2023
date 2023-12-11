4398a31aae268242a888caf04156c4505171e8f1
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-11 13:56:04 +0100

    day 11 pass expansion factor to make both exercises pass

--- 11/functions_day_11.py
+++ 11/functions_day_11.py
@@ -24,28 +24,28 @@ def find_expanding_columns(data: np.ndarray) -> list[int]:
     return expanding_columns
 
 
-def find_distance(galaxy_a: tuple[int, int], galaxy_b: tuple[int, int], expanding_lines: list[int], expanding_columns: list[int]):
+def find_distance(galaxy_a: tuple[int, int], galaxy_b: tuple[int, int], expanding_lines: list[int], expanding_columns: list[int], expansion_factor: int) -> int:
     line_coords_ordered = sorted([galaxy_a[0], galaxy_b[0]])
     column_coords_ordered = sorted([galaxy_a[1], galaxy_b[1]])
     distance = line_coords_ordered[1] - line_coords_ordered[0] + column_coords_ordered[1] - column_coords_ordered[0]
-    distance += sum([line_coords_ordered[0] < line < line_coords_ordered[1] for line in expanding_lines])*(1000000-1)
-    distance += sum([column_coords_ordered[0] < column < column_coords_ordered[1] for column in expanding_columns])*(1000000-1)
+    distance += sum([line_coords_ordered[0] < line < line_coords_ordered[1] for line in expanding_lines])*(expansion_factor-1)
+    distance += sum([column_coords_ordered[0] < column < column_coords_ordered[1] for column in expanding_columns])*(expansion_factor-1)
     return distance
 
 
-def find_all_distances_between_galaxies(galaxy_coords: tuple[list[int], list[int]], expanding_lines: list[int], expanding_columns: list[int]) -> list[int]:
+def find_all_distances_between_galaxies(galaxy_coords: tuple[list[int], list[int]], expanding_lines: list[int], expanding_columns: list[int], expansion_factor: int) -> list[int]:
     distances = []
     for galaxy_a_idx in range(0, len(galaxy_coords[0])):
         galaxy_a = (galaxy_coords[0][galaxy_a_idx], galaxy_coords[1][galaxy_a_idx])
         for galaxy_b_idx in range(galaxy_a_idx + 1, len(galaxy_coords[0])):
             galaxy_b = (galaxy_coords[0][galaxy_b_idx], galaxy_coords[1][galaxy_b_idx])
-            distances.append(find_distance(galaxy_a, galaxy_b, expanding_lines, expanding_columns))
+            distances.append(find_distance(galaxy_a, galaxy_b, expanding_lines, expanding_columns, expansion_factor))
     return distances
 
 
-def exercise_1(data: np.ndarray) -> int:
+def exercise(data: np.ndarray, expansion_factor: int) -> int:
     expanding_lines = find_expanding_lines(data)
     expanding_columns = find_expanding_columns(data)
     galaxy_coords = np.where(data == '#')
-    distances = find_all_distances_between_galaxies(galaxy_coords, expanding_lines, expanding_columns)
+    distances = find_all_distances_between_galaxies(galaxy_coords, expanding_lines, expanding_columns, expansion_factor)
     return sum(distances)
--- 11/test_day_11.py
+++ 11/test_day_11.py
@@ -13,18 +13,22 @@ def test_find_expanding_columns():
 
 
 def test_find_distance():
-    assert find_distance((0, 3), (1, 7), [3, 7], [2, 5, 8]) == 6
-    assert find_distance((0, 3), (2, 0), [3, 7], [2, 5, 8]) == 6
-    assert find_distance((1, 7), (2, 0), [3, 7], [2, 5, 8]) == 10
+    assert find_distance((0, 3), (1, 7), [3, 7], [2, 5, 8], 2) == 6
+    assert find_distance((0, 3), (2, 0), [3, 7], [2, 5, 8], 2) == 6
+    assert find_distance((1, 7), (2, 0), [3, 7], [2, 5, 8], 2) == 10
 
 
 def test_find_all_distances_between_galaxies():
-    assert find_all_distances_between_galaxies(([0, 1, 2], [3, 7, 0]), [], [2, 5, 8]) == [6, 6, 10]
+    assert find_all_distances_between_galaxies(([0, 1, 2], [3, 7, 0]), [], [2, 5, 8], 2) == [6, 6, 10]
 
 
 def test_exercise_1():
-    assert exercise_1(data_small) == 374
+    assert exercise(data_small, 2) == 374
+
+
+def test_exercise_1_all_data():
+    assert exercise(data_full, 2) == 9565386
 
 
 def test_exercise_2_all_data():
-    assert exercise_1(data_full) == 857986849428
+    assert exercise(data_full, 1000000) == 857986849428
