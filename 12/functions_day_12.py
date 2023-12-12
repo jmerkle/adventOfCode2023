116a97baf034c7ae6478dd90375f425e0c41e594
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-12 08:48:38 +0100

    day 12 exercise 1 done, but slow

--- 12/functions_day_12.py
+++ 12/functions_day_12.py
@@ -1,9 +1,38 @@
+import re
+
 def read_file_as_list_of_lines_and_filter_empty_lines(filename: str):
     f = open(filename, 'r')
     data = f.read()
     f.close()
     return [x for x in data.split("\n") if len(x) > 0]
 
+def validate_arrangement(line: str) -> bool:
+    record, check_values = line.split(" ")
+    groups_of_hash = [len(group) for group in re.findall("#+", record)]
+    check_values = [int(c) for c in check_values.split(",")]
+    return groups_of_hash == check_values
+
+
+def apply_arrangement(line: str, arrangement: str) -> str:
+    for i in arrangement:
+        if i=="1":
+            line = line.replace("?", "#", 1)
+        else:
+            line = line.replace("?", ".", 1)
+    return line
+
+
+def to_binary(number: int, num_digits: int) -> str:
+    return format(number, '0'+str(num_digits)+'b')
+
+
+def count_possible_arrangements(line: str) -> int:
+    num_unknowns = sum([c == "?" for c in line])
+    possible_arrangements = 0
+    for i in range(0, 2**num_unknowns):
+        if validate_arrangement(apply_arrangement(line, to_binary(i, num_unknowns))):
+            possible_arrangements += 1
+    return possible_arrangements
 
 def exercise_1(data: list[str]) -> int:
-    return 0
+    return sum(list(map(count_possible_arrangements, data)))
--- 12/test_day_12.py
+++ 12/test_day_12.py
@@ -4,5 +4,30 @@ data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt'
 data_full = read_file_as_list_of_lines_and_filter_empty_lines('input.txt')
 
 
+def test_validate_arrangement():
+    assert validate_arrangement("#.#.### 1,1,3") == True
+    assert validate_arrangement("##..### 1,1,3") == False
+    assert validate_arrangement(".###.##.#... 3,2,1") == True
+    assert validate_arrangement(".###..##..#. 3,2,1") == True
+    assert validate_arrangement(".###..###... 3,2,1") == False
+
+
+def test_apply_arrangement():
+    assert apply_arrangement("???.### 1,1,3", "101") == "#.#.### 1,1,3"
+
+
+def test_find_arrangements():
+    assert count_possible_arrangements("???.### 1,1,3") == 1
+    assert count_possible_arrangements(".??..??...?##. 1,1,3") == 4
+    assert count_possible_arrangements("?#?#?#?#?#?#?#? 1,3,1,6") == 1
+    assert count_possible_arrangements("????.#...#... 4,1,1") == 1
+    assert count_possible_arrangements("????.######..#####. 1,6,5") == 4
+    assert count_possible_arrangements("?###???????? 3,2,1") == 10
+
+
 def test_exercise_1():
     assert exercise_1(data_small) == 21
+
+
+def test_exercise_1_full_data():
+    assert exercise_1(data_full) == 7771
