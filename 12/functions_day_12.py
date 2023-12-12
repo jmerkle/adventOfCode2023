671146e5026b525550ed092b9aa339909f93b86e
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-12 23:36:23 +0100

    day 12 exercise 2 rewrite recursively. How is this still too slow?

--- 12/functions_day_12.py
+++ 12/functions_day_12.py
@@ -9,50 +9,59 @@ def read_file_as_list_of_lines_and_filter_empty_lines(filename: str):
     return [x for x in data.split("\n") if len(x) > 0]
 
 
-def validate_arrangement(line: str) -> bool:
-    record, check_values = line.split(" ")
-    groups_of_hash = [len(group) for group in re.findall("#+", record)]
-    check_values = [int(c) for c in check_values.split(",")]
-    return groups_of_hash == check_values
-
-
-def find_all_q(line: list[str]) -> list[int]:
-    return [i for i in range(len(line)) if line[i] == "?"]
-
-
-def apply_arrangement(line: str, arrangement: list[int]) -> str:
-    line_as_list = list(line)
-    indexes_of_q = find_all_q(line_as_list)
-    for i in arrangement:
-        line_as_list[indexes_of_q[i]] = "#"
-    return "".join(line_as_list).replace("?", ".")
-
-
-def to_binary(number: int, num_digits: int) -> str:
-    return format(number, '0' + str(num_digits) + 'b')
-
-
-def count_possible_arrangements(line: str) -> int:
-    record, check_values = line.split(" ")
-    num_unknowns = sum([c == "?" for c in record])
-    num_unplaced_hashs = sum([int(c) for c in check_values.split(",")]) - sum([c == "#" for c in record])
-    possible_arrangements = 0
-    for c in itertools.combinations(range(0, num_unknowns), num_unplaced_hashs):
-        if validate_arrangement(apply_arrangement(line, c)):
-            possible_arrangements += 1
-    return possible_arrangements
-
-
-def exercise_1(data: list[str]) -> int:
-    return sum(list(map(count_possible_arrangements, data)))
-
-
 def unfold_record(line: str) -> str:
     factor = 5
     record, check_values = line.split(" ")
     unfolded_record = "?".join([record] * factor)
     unfolded_check_values = ",".join([check_values] * factor)
     return unfolded_record + " " + unfolded_check_values
+
+
+def record_could_start_with(record: str, check_value: int) -> bool:
+    if "." not in record[0:check_value]:
+        if len(record) > check_value and record[check_value] == "#":
+            return False
+        return True
+    return False
+
+
+def generate_possible_remaining_records(record: str, check_value: int) -> list[str]:
+    if len(record) < check_value:
+        return []
+    if len(record) == check_value:
+        if record_could_start_with(record, check_value):
+            return [""]
+        else:
+            return []
+    remaining = []
+    if record_could_start_with(record, check_value):
+        remaining = [record[check_value+1:]]
+    if record[0] == "#":
+        return remaining
+    else:
+        return remaining + generate_possible_remaining_records(record[1:], check_value)
+
+
+def count_possible_arrangements(line: str) -> int:
+    record, check_values_str = remove_duplicate_dots(line).split(" ")
+    check_values = [int(c) for c in check_values_str.split(",")]
+    possible_remaining_records = generate_possible_remaining_records(record, check_values[0])
+    if len(check_values) == 1:
+        return len(list(filter(lambda r: "#" not in r, possible_remaining_records)))
+    counter = 0
+    for r in possible_remaining_records:
+        counter += count_possible_arrangements(r + " " + ",".join([str(i) for i in check_values[1:]]))
+    return counter
+
+
+def remove_duplicate_dots(line: str) -> str:
+    while line.replace("..", ".") != line:
+        line = line.replace("..", ".")
+    return line
+
+
+def exercise_1(data: list[str]) -> int:
+    return sum(list(map(count_possible_arrangements, data)))
 
 
 def exercise_2(data: list[str]) -> int:
--- 12/test_day_12.py
+++ 12/test_day_12.py
@@ -4,29 +4,51 @@ data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt'
 data_full = read_file_as_list_of_lines_and_filter_empty_lines('input.txt')
 
 
-def test_validate_arrangement():
-    assert validate_arrangement("#.#.### 1,1,3") == True
-    assert validate_arrangement("##..### 1,1,3") == False
-    assert validate_arrangement(".###.##.#... 3,2,1") == True
-    assert validate_arrangement(".###..##..#. 3,2,1") == True
-    assert validate_arrangement(".###..###... 3,2,1") == False
-
-
-def test_find_all_q():
-    assert find_all_q(list("?#?#?#?#?#?#??#?")) == [0, 2, 4, 6, 8, 10, 12, 13, 15]
-
-
-def test_apply_arrangement():
-    assert apply_arrangement("???.### 1,1,3", (0, 2)) == "#.#.### 1,1,3"
-
-
 def test_find_arrangements():
+    assert count_possible_arrangements("?.?????#???#? 1,1,2,2") == 22
+    assert count_possible_arrangements("#???#?? 1") == 0
+
+    assert count_possible_arrangements("??# 1") == 1
     assert count_possible_arrangements("???.### 1,1,3") == 1
     assert count_possible_arrangements(".??..??...?##. 1,1,3") == 4
     assert count_possible_arrangements("?#?#?#?#?#?#?#? 1,3,1,6") == 1
     assert count_possible_arrangements("????.#...#... 4,1,1") == 1
     assert count_possible_arrangements("????.######..#####. 1,6,5") == 4
     assert count_possible_arrangements("?###???????? 3,2,1") == 10
+    assert count_possible_arrangements("????.#????#?????#??# 1,2,1,1,7") == 5
+
+
+def test_unfold_record():
+    assert unfold_record(".# 1") == ".#?.#?.#?.#?.# 1,1,1,1,1"
+
+
+def test_find_arrangements_unfolded():
+    assert count_possible_arrangements(unfold_record("?.?????#???#? 1,1,2,2")) == 1
+    assert count_possible_arrangements(unfold_record("???.### 1,1,3")) == 1
+    assert count_possible_arrangements(unfold_record(".??..??...?##. 1,1,3")) == 16384
+    assert count_possible_arrangements(unfold_record("?#?#?#?#?#?#?#? 1,3,1,6")) == 1
+    assert count_possible_arrangements(unfold_record("????.#...#... 4,1,1")) == 16
+    assert count_possible_arrangements(unfold_record("????.######..#####. 1,6,5")) == 2500
+    assert count_possible_arrangements(unfold_record("?###???????? 3,2,1")) == 506250
+
+
+def test_record_could_start_with():
+    assert record_could_start_with("????.", 1)
+    assert record_could_start_with("??.", 1)
+    assert record_could_start_with("??.", 2)
+    assert not record_could_start_with("?#", 1)
+    assert not record_could_start_with("??#", 2)
+    assert not record_could_start_with("?.", 2)
+
+
+def test_generate_possible_remaining_records():
+    assert generate_possible_remaining_records("#", 1) == [""]
+    assert generate_possible_remaining_records(".", 1) == []
+    assert generate_possible_remaining_records("??#", 1) == ["#", ""]
+    assert generate_possible_remaining_records("???#?????#??#", 1) == ["?#?????#??#", "#?????#??#", "????#??#"]
+    assert generate_possible_remaining_records("?#?#?#?#?#?#?#?", 1) == ["#?#?#?#?#?#?"]
+    assert generate_possible_remaining_records("?#?#?#?#?#?#?#?", 2) == ["#?#?#?#?#?#?"]
+    assert generate_possible_remaining_records("?#?#?#?#?#?#?#?", 3) == ["#?#?#?#?#?"]
 
 
 def test_exercise_1():
@@ -37,18 +59,9 @@ def test_exercise_1_full_data():
     assert exercise_1(data_full) == 7771
 
 
-def test_unfold_record():
-    assert unfold_record(".# 1") == ".#?.#?.#?.#?.# 1,1,1,1,1"
-
-
-def test_find_arrangements_unfolded():
-    assert count_possible_arrangements(unfold_record("???.### 1,1,3")) == 1
-    assert count_possible_arrangements(unfold_record(".??..??...?##. 1,1,3")) == 16384
-    assert count_possible_arrangements(unfold_record("?#?#?#?#?#?#?#? 1,3,1,6")) == 1
-    assert count_possible_arrangements(unfold_record("????.#...#... 4,1,1")) == 16
-    assert count_possible_arrangements(unfold_record("????.######..#####. 1,6,5")) == 2500
-    assert count_possible_arrangements(unfold_record("?###???????? 3,2,1")) == 506250
-
-
 def test_exercise_2():
     assert exercise_2(data_small) == 525152
+
+
+def test_exercise_2_full_data():
+    assert exercise_2(data_full) == 525152
