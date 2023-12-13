7f9f6ae7134879eb47b62360d1dde79bfb9b3dfa
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-13 14:00:03 +0100

    day 13 exercise 1 done

--- 13/functions_day_13.py
+++ 13/functions_day_13.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def read_file_as_list_of_sections(filename: str) -> list[list[str]]:
     f = open(filename, 'r')
     data = f.read()
@@ -5,5 +8,35 @@ def read_file_as_list_of_sections(filename: str) -> list[list[str]]:
     return [x.strip().split("\n") for x in data.split("\n\n") if len(x) > 0]
 
 
+def split_and_compare(l: list[int], split_at: int) -> bool:
+    if split_at == 0:
+        return False
+    left = list(reversed(l[:split_at]))
+    right = l[split_at:]
+    if len(left) > len(right):
+        return left[: len(right)] == right
+    else:
+        return left == right[: len(left)]
+
+
+def find_middle(l: list[int]) -> int:
+    middle = 0
+    while not split_and_compare(l, middle):
+        middle += 1
+        if middle >= len(l):
+            return -1
+    return middle
+
+
+def process_pattern(pattern: list[str]) -> int:
+    matrix = np.array(list(map(lambda line: list(line), pattern)))
+    _, order_rows = np.unique(matrix, axis=0, return_inverse=True)
+    horizontal = find_middle(list(order_rows))
+    if horizontal > 0:
+        return horizontal*100
+    _, order_columns = np.unique(matrix, axis=1, return_inverse=True)
+    return find_middle(list(order_columns))
+
+
 def exercise_1(data: list[list[str]]) -> int:
-    return 0
+    return sum(list(map(process_pattern, data)))
--- 13/test_day_13.py
+++ 13/test_day_13.py
@@ -4,5 +4,23 @@ data_small = read_file_as_list_of_sections('input_small.txt')
 data_full = read_file_as_list_of_sections('input.txt')
 
 
+def test_split_and_compare():
+    assert not split_and_compare([1, 1, 2], 0)
+    assert split_and_compare([1, 1, 2], 1)
+
+
+def test_find_middle():
+    assert find_middle([1, 1]) == 1
+    assert find_middle([1, 2]) == -1
+    assert find_middle([1, 1, 2]) == 1
+    assert find_middle([2, 1, 1]) == 2
+    assert find_middle([2, 1, 1]) == 2
+    assert find_middle([1, 2, 2, 2, 1, 1]) == 5
+
+
 def test_exercise_1():
     assert exercise_1(data_small) == 405
+
+
+def test_exercise_1_full_data():
+    assert exercise_1(data_full) == 27505
