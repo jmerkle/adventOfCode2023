f666a968a2743a43cdd1da42eda45471ef57d52c
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-14 00:41:33 +0100

    day 13 exercise 2 done

--- 13/functions_day_13.py
+++ 13/functions_day_13.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -28,15 +30,51 @@ def find_middle(l: list[int]) -> int:
     return middle
 
 
+def find_multiple_middles(l: list[int]) -> list[int]:
+    middles = [middle for middle in range(0, len(l)) if split_and_compare(l, middle)]
+    return middles
+
+
 def process_pattern(pattern: list[str]) -> int:
-    matrix = np.array(list(map(lambda line: list(line), pattern)))
+    return process_matrix(np.array(list(map(lambda line: list(line), pattern))))
+
+
+def process_matrix(matrix: np.ndarray, ignore_value: int = -1) -> int:
     _, order_rows = np.unique(matrix, axis=0, return_inverse=True)
-    horizontal = find_middle(list(order_rows))
-    if horizontal > 0:
-        return horizontal*100
+    horizontal = find_multiple_middles(list(order_rows))
     _, order_columns = np.unique(matrix, axis=1, return_inverse=True)
-    return find_middle(list(order_columns))
+    vertical = find_multiple_middles(list(order_columns))
+    all_values = [v*100 for v in horizontal] + vertical
+    if len(all_values) > 1 and all_values[0] == ignore_value:
+        return all_values[1]
+    if len(all_values) > 0:
+        return all_values[0]
+    return -1
+
+
+def smudge_character(matrix: np.ndarray, character_index: int) -> np.ndarray:
+    row, column = math.floor(character_index/matrix.shape[1]), character_index % matrix.shape[1]
+    if matrix[row, column] == ".":
+        matrix[row, column] = "#"
+    else:
+        matrix[row, column] = "."
+    return matrix
+
+
+def process_smudged_pattern(pattern: list[str]) -> int:
+    unsmudged_value = process_pattern(pattern)
+    matrix = np.array(list(map(lambda line: list(line), pattern)))
+    smudged_character_index = 0
+    smudged_value = -1
+    while smudged_value in [-1, unsmudged_value]:
+        smudged_value = process_matrix(smudge_character(matrix.copy(), smudged_character_index), unsmudged_value)
+        smudged_character_index += 1
+    return smudged_value
 
 
 def exercise_1(data: list[list[str]]) -> int:
     return sum(list(map(process_pattern, data)))
+
+
+def exercise_2(data: list[list[str]]) -> int:
+    return sum(list(map(process_smudged_pattern, data)))
--- 13/test_day_13.py
+++ 13/test_day_13.py
@@ -18,9 +18,126 @@ def test_find_middle():
     assert find_middle([1, 2, 2, 2, 1, 1]) == 5
 
 
+def test_find_multiple_middles():
+    assert find_multiple_middles([1, 1]) == [1]
+    assert find_multiple_middles([1, 2]) == []
+    assert find_multiple_middles([1, 1, 2]) == [1]
+    assert find_multiple_middles([2, 1, 1]) == [2]
+    assert find_multiple_middles([2, 1, 1]) == [2]
+    assert find_multiple_middles([1, 2, 2, 2, 1, 1]) == [5]
+    assert find_multiple_middles([1, 1, 1, 1, 1, 1]) == [1, 2, 3, 4, 5]
+
+
 def test_exercise_1():
     assert exercise_1(data_small) == 405
 
 
 def test_exercise_1_full_data():
     assert exercise_1(data_full) == 27505
+
+
+def test_smudge_character():
+    matrix = np.array([list("#.##..##."), list("..#.##.#.")])
+    assert (smudge_character(matrix.copy(), 0) == np.array([list("..##..##."), list("..#.##.#.")])).all()
+    assert (smudge_character(matrix.copy(), 1) == np.array([list("####..##."), list("..#.##.#.")])).all()
+    assert (smudge_character(matrix.copy(), 8) == np.array([list("#.##..###"), list("..#.##.#.")])).all()
+    assert (smudge_character(matrix.copy(), 9) == np.array([list("#.##..##."), list("#.#.##.#.")])).all()
+    assert (smudge_character(matrix.copy(), 10) == np.array([list("#.##..##."), list(".##.##.#.")])).all()
+
+
+def test_manually_process_smudged_pattern():
+    matrix1 = np.array([
+        list("#.##..##."),
+        list("..#.##.#."),
+        list("##......#"),
+        list("##......#"),
+        list("..#.##.#."),
+        list("..##..##."),
+        list("#.#.##.#."),
+            ])
+
+    assert process_matrix(matrix1.copy()) == 5
+    assert process_matrix(smudge_character(matrix1.copy(), 0)) == 300
+
+    matrix2 = np.array([
+        list("#...##..#"),
+        list("#....#..#"),
+        list("..##..###"),
+        list("#####.##."),
+        list("#####.##."),
+        list("..##..###"),
+        list("#....#..#"),
+    ])
+
+    assert process_matrix(matrix2.copy()) == 400
+    assert process_matrix(smudge_character(matrix2.copy(), 13)) == 100
+
+
+def test_process_smudged_pattern():
+    pattern1 = [
+        "#.##..##.",
+        "..#.##.#.",
+        "##......#",
+        "##......#",
+        "..#.##.#.",
+        "..##..##.",
+        "#.#.##.#.",
+    ]
+
+    assert process_smudged_pattern(pattern1) == 300
+
+    pattern2 = [
+        "#...##..#",
+        "#....#..#",
+        "..##..###",
+        "#####.##.",
+        "#####.##.",
+        "..##..###",
+        "#....#..#",
+    ]
+
+    assert process_smudged_pattern(pattern2) == 100
+
+    pattern3 = [
+        "..#.#....#....#",
+        "..#.#....#....#",
+        "..##.#..#..##..",
+        "#.#.##.#.#.##.#",
+        "####.###.#.##.#",
+        ".####.######.##",
+        "##.#....#.####.",
+        ".####.###.#..#.",
+        "#..##...#.#..#.",
+    ]
+
+    assert process_smudged_pattern(pattern3) == 12
+
+    pattern4 = [
+        "##....##.#.",
+        "##.##.#..#.",
+        "..####....#",
+        "#######..##",
+        "##..#......",
+        "...##......",
+        "###....##..",
+        "..#.#..##..",
+        "...#.#....#",
+        "..##.......",
+        "..##.#.##.#",
+        "##...##..##",
+        "######.##.#",
+        "###...#..#.",
+        "...###....#",
+        "..##.......",
+        "###.##....#",
+    ]
+
+    assert process_smudged_pattern(pattern4) == 8
+
+
+def test_exercise_2():
+    assert exercise_2(data_small) == 400
+
+
+def test_exercise_2_full_data():
+    assert exercise_2(data_full) == 22906
