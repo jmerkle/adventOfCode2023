febd2c08cc5b4b5af5456c38d8565d10e1538681
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-14 19:25:11 +0100

    day 14 exercise 1 tilt platform

--- 14/functions_day_14.py
+++ 14/functions_day_14.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def read_file_as_list_of_lines_and_filter_empty_lines(filename: str):
     f = open(filename, 'r')
@@ -6,6 +7,33 @@ def read_file_as_list_of_lines_and_filter_empty_lines(filename: str):
     return [x for x in data.split("\n") if len(x) > 0]
 
 
+def as_matrix(data: list[str]) -> np.ndarray:
+    return np.array(list(map(lambda line: list(line), data)))
+
+
+def rotate_matrix_so_direction_points_right(matrix: np.ndarray, direction: str) -> np.ndarray:
+    return np.rot90(matrix, 3)
+
+
+def flatten_list(xss):
+    return [x for xs in xss for x in xs]
+
+
+def tilt_line(line: np.ndarray) -> np.ndarray:
+    # TODO: this code hurts
+    split_line = np.split(line.flatten(), np.where(line.flatten() == "#")[0])
+    sorted_splits = [sorted(split) for split in split_line]
+    return np.array(flatten_list(sorted_splits))
+
+
+def tilt_platform(matrix: np.ndarray, direction: str) -> np.ndarray:
+    rotated = rotate_matrix_so_direction_points_right(matrix, direction)
+    for line in range(0, rotated.shape[0]):
+        rotated[line] = tilt_line(rotated[line])
+    return matrix
+
 def exercise_1(data: list[str]) -> int:
+    platform = as_matrix(data)
+    tilted = tilt_platform(platform, "n")
     return 0
 
--- 14/test_day_14.py
+++ 14/test_day_14.py
@@ -4,5 +4,65 @@ data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt'
 data_full = read_file_as_list_of_lines_and_filter_empty_lines('input.txt')
 
 
+def test_tilt_line():
+    line = as_matrix(["O....#....##.O.O.##"])
+
+    expected = as_matrix(["....O#....##...OO##"])
+
+    assert (tilt_line(line) == expected).all()
+
+
+def test_rotate_matrix_so_direction_is_right():
+    platform = as_matrix([
+        "O....#....",
+        "O.OO#....#",
+        ".....##...",
+        "OO.#O....O",
+        ".O.....O#.",
+        "O.#..O.#.#",
+        "..O..#O..O",
+        ".......O..",
+        "#....###..",
+        "#OO..#....",
+    ])
+
+    rotated = rotate_matrix_so_direction_points_right(platform, "n")
+
+    assert platform[0, 0] == rotated[0, -1] == "O"
+    assert platform[0, -1] == rotated[-1, -1] == "."
+    assert platform[-1, 0] == rotated[0, 0] == "#"
+    assert platform[-1, -1] == rotated[-1, 0] == "."
+
+
+def test_tilt_platform():
+    platform = as_matrix([
+        "O....#....",
+        "O.OO#....#",
+        ".....##...",
+        "OO.#O....O",
+        ".O.....O#.",
+        "O.#..O.#.#",
+        "..O..#O..O",
+        ".......O..",
+        "#....###..",
+        "#OO..#....",
+    ])
+
+    expected = as_matrix([
+        "OOOO.#.O..",
+        "OO..#....#",
+        "OO..O##..O",
+        "O..#.OO...",
+        "........#.",
+        "..#....#.#",
+        "..O..#.O.O",
+        "..O.......",
+        "#....###..",
+        "#....#....",
+    ])
+
+    assert (tilt_platform(platform, "n") == expected).all()
+
+
 def test_exercise_1():
     assert exercise_1(data_small) == 136
