de2aa5db255a8e53b1befeda0a6b7894b418adf3
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-15 22:33:51 +0100

    day 15 exercise 2 done

--- 15/functions_day_15.py
+++ 15/functions_day_15.py
@@ -12,7 +12,7 @@ def read_file_and_split_on_comma(filename: str):
 def hash_function(input_string: str) -> int:
     current_value = 0
     for c in list(input_string):
-        current_value = (current_value + ord(c))*17 % 256
+        current_value = (current_value + ord(c)) * 17 % 256
     return current_value
 
 
@@ -20,14 +20,15 @@ def exercise_1(data: list[str]) -> int:
     return sum(list(map(hash_function, data)))
 
 
-def box_and_operation(step_input: str) -> tuple[int, Optional[int]]:
-    if step_input[-1] == "-":
-        return hash_function(step_input[:-1]), None
-    label, focal_length = step_input.split("=")
-    return hash_function(label), int(focal_length)
-
 LabeledLens: TypeAlias = tuple[str, int]
 Box: TypeAlias = list[LabeledLens]
+
+
+def box_number_and_operation(step_input: str) -> tuple[int, str, Optional[int]]:
+    if step_input[-1] == "-":
+        return hash_function(step_input[:-1]), step_input[:-1], None
+    label, focal_length = step_input.split("=")
+    return hash_function(label), label, int(focal_length)
 
 
 def find_labeled_lens_in_box(box: Box, label: str) -> Optional[int]:
@@ -37,7 +38,7 @@ def find_labeled_lens_in_box(box: Box, label: str) -> Optional[int]:
 def remove_from_box(box: Box, label: str) -> Box:
     index_of_lens_with_label = find_labeled_lens_in_box(box, label)
     if index_of_lens_with_label is not None:
-        del(box[index_of_lens_with_label])
+        del (box[index_of_lens_with_label])
     return box
 
 
@@ -50,16 +51,30 @@ def add_to_box(box: Box, lens: LabeledLens) -> Box:
     return box
 
 
-def perform_step(boxes: list[Box], step_input: str) -> list[list[str]]:
-    return []
+def perform_step(boxes: list[Box], step_input: str):
+    box_number, label, focal_length = box_number_and_operation(step_input)
+    if focal_length is None:
+        remove_from_box(boxes[box_number], label)
+    else:
+        add_to_box(boxes[box_number], (label, focal_length))
+
+
+def focal_power_box(box: Box, box_number: int) -> int:
+    power = 0
+    for slot in range(len(box)):
+        power += (box_number + 1) * (slot + 1) * box[slot][1]
+    return power
 
 
 def focal_power(boxes: list[Box]) -> int:
-    return 0
+    power = 0
+    for b in range(len(boxes)):
+        power += focal_power_box(boxes[b], b)
+    return power
 
 
 def exercise_2(data: list[str]) -> int:
     boxes = [[] for _ in range(256)]
     for step_input in data:
-        boxes = perform_step(boxes, step_input)
+        perform_step(boxes, step_input)
     return focal_power(boxes)
--- 15/test_day_15.py
+++ 15/test_day_15.py
@@ -18,18 +18,18 @@ def test_exercise_1_full_data():
     assert exercise_1(data_full) == 511498
 
 
-def test_box_and_operation():
-    assert box_and_operation("rn=1") == (hash_function("rn"), 1)
-    assert box_and_operation("cm-") == (hash_function("cm"), None)
-    assert box_and_operation("qp=3") == (hash_function("qp"), 3)
-    assert box_and_operation("cm=2") == (hash_function("cm"), 2)
-    assert box_and_operation("qp-") == (hash_function("qp"), None)
-    assert box_and_operation("pc=4") == (hash_function("pc"), 4)
-    assert box_and_operation("ot=9") == (hash_function("ot"), 9)
-    assert box_and_operation("ab=5") == (hash_function("ab"), 5)
-    assert box_and_operation("pc-") == (hash_function("pc"), None)
-    assert box_and_operation("pc=6") == (hash_function("pc"), 6)
-    assert box_and_operation("ot=7") == (hash_function("ot"), 7)
+def test_box_number_and_labeled_lens():
+    assert box_number_and_operation("rn=1") == (hash_function("rn"), "rn", 1)
+    assert box_number_and_operation("cm-") == (hash_function("cm"), "cm", None)
+    assert box_number_and_operation("qp=3") == (hash_function("qp"), "qp", 3)
+    assert box_number_and_operation("cm=2") == (hash_function("cm"), "cm", 2)
+    assert box_number_and_operation("qp-") == (hash_function("qp"), "qp", None)
+    assert box_number_and_operation("pc=4") == (hash_function("pc"), "pc", 4)
+    assert box_number_and_operation("ot=9") == (hash_function("ot"), "ot", 9)
+    assert box_number_and_operation("ab=5") == (hash_function("ab"), "ab", 5)
+    assert box_number_and_operation("pc-") == (hash_function("pc"), "pc", None)
+    assert box_number_and_operation("pc=6") == (hash_function("pc"), "pc", 6)
+    assert box_number_and_operation("ot=7") == (hash_function("ot"), "ot", 7)
 
 
 def test_add_to_empty_box():
@@ -58,3 +58,7 @@ def test_remove_from_box():
 
 def test_exercise_2():
     assert exercise_2(data_small) == 145
+
+
+def test_exercise_2_full_data():
+    assert exercise_2(data_full) == 284674
