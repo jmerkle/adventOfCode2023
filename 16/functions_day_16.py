38c0eb1df2fc2e85b5ebe27bbd55af83bc0297fc
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-16 13:43:24 +0100

    day 16 exercise 1 add queue

--- 16/functions_day_16.py
+++ 16/functions_day_16.py
@@ -1,5 +1,6 @@
+import queue
 from enum import Enum
-from typing import TypeAlias
+from typing import TypeAlias, Optional
 
 
 class Direction(Enum):
@@ -11,7 +12,24 @@ class Direction(Enum):
 
 GridCoordinates: TypeAlias = tuple[int, int]
 Grid: TypeAlias = list[list[str]]
+EnergizedGrid: TypeAlias = list[list[str]]
 BeamMovement: TypeAlias = tuple[Direction, GridCoordinates]
+
+
+class MovementQueue:
+    movement_queue = queue.Queue()
+    already_queued_movements = set()
+
+    def put(self, movement: BeamMovement) -> None:
+        if movement not in self.already_queued_movements:
+            self.movement_queue.put(movement)
+            self.already_queued_movements.add(movement)
+
+    def get(self) -> Optional[BeamMovement]:
+        try:
+            return self.movement_queue.get_nowait()
+        except queue.Empty:
+            return None
 
 
 def read_file_as_list_of_list_and_filter_empty_lines(filename: str) -> Grid:
@@ -21,5 +39,17 @@ def read_file_as_list_of_list_and_filter_empty_lines(filename: str) -> Grid:
     return [list(x) for x in data.split("\n") if len(x) > 0]
 
 
-def exercise_1(data: Grid) -> int:
+def energize(grid: Grid, start_movement: BeamMovement) -> EnergizedGrid:
+
+
+    return []
+
+
+def count_energized_tiles(grid: EnergizedGrid) -> int:
     return 0
+
+
+def exercise_1(data: Grid) -> int:
+    start_movement = (Direction.RIGHT, (0, 0))
+    energized_grid = energize(data, start_movement)
+    return count_energized_tiles(energized_grid)
--- 16/test_day_16.py
+++ 16/test_day_16.py
@@ -4,5 +4,21 @@ data_small = read_file_as_list_of_list_and_filter_empty_lines('input_small.txt')
 data_full = read_file_as_list_of_list_and_filter_empty_lines('input.txt')
 
 
+def test_movement_queue():
+    q = MovementQueue()
+    movement_1 = Direction.RIGHT, (0, 0)
+    movement_2 = Direction.LEFT, (0, 0)
+    q.put(movement_1)
+    q.put(movement_2)
+    q.put(movement_1)
+
+    result1 = q.get()
+    result2 = q.get()
+    result3 = q.get()
+    assert result1 == movement_1
+    assert result2 == movement_2
+    assert result3 is None
+
+
 def test_exercise_1():
     assert exercise_1(data_small) == 46
