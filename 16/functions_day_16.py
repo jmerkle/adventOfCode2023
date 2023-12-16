45463765064bb082b65af4c31e550ef5e055e02d
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-16 15:17:32 +0100

    day 16 exercise 1 move and calculate next movements

--- 16/functions_day_16.py
+++ 16/functions_day_16.py
@@ -20,6 +20,10 @@ class MovementQueue:
     movement_queue = queue.Queue()
     already_queued_movements = set()
 
+    def __init__(self):
+        self.movement_queue = queue.Queue()
+        self.already_queued_movements = set()
+
     def put(self, movement: BeamMovement) -> None:
         if movement not in self.already_queued_movements:
             self.movement_queue.put(movement)
@@ -39,14 +43,93 @@ def read_file_as_list_of_list_and_filter_empty_lines(filename: str) -> Grid:
     return [list(x) for x in data.split("\n") if len(x) > 0]
 
 
+def walk_in_direction(grid: Grid, coords: GridCoordinates, direction: Direction) -> Optional[BeamMovement]:
+    line, column = coords
+    match direction:
+        case Direction.UP:
+            line -= 1
+        case Direction.RIGHT:
+            column += 1
+        case Direction.DOWN:
+            line += 1
+        case Direction.LEFT:
+            column -= 1
+    if tile_is_on_grid(grid, (line, column)):
+        return direction, (line, column)
+    return None
+
+
+def tile_is_on_grid(grid: Grid, coords: GridCoordinates) -> bool:
+    line, column = coords
+    return all([line >= 0, column >= 0, line < len(grid), column < len(grid[0])])
+
+
+def calculate_more_movements(grid: Grid, movement: BeamMovement) -> list[BeamMovement]:
+    direction, (line, column) = movement
+    tile_type = grid[line][column]
+    new_directions = calculate_new_directions(tile_type, direction)
+    new_movements = [walk_in_direction(grid, (line, column), new_direction) for new_direction in new_directions]
+    return list(filter(lambda x: x is not None, new_movements))
+
+
+def calculate_new_directions(tile_symbol: str, direction: Direction) -> list[Direction]:
+    match tile_symbol, direction:
+        case ".", _:
+            return [direction]
+
+        case "/", Direction.RIGHT:
+            return [Direction.UP]
+        case "/", Direction.DOWN:
+            return [Direction.LEFT]
+        case "/", Direction.LEFT:
+            return [Direction.DOWN]
+        case "/", Direction.UP:
+            return [Direction.RIGHT]
+
+        case "\\", Direction.RIGHT:
+            return [Direction.DOWN]
+        case "\\", Direction.DOWN:
+            return [Direction.RIGHT]
+        case "\\", Direction.LEFT:
+            return [Direction.UP]
+        case "\\", Direction.UP:
+            return [Direction.LEFT]
+
+        case "-", Direction.RIGHT:
+            return [Direction.RIGHT]
+        case "-", Direction.DOWN:
+            return [Direction.RIGHT, Direction.LEFT]
+        case "-", Direction.LEFT:
+            return [Direction.LEFT]
+        case "-", Direction.UP:
+            return [Direction.RIGHT, Direction.LEFT]
+
+        case "|", Direction.RIGHT:
+            return [Direction.UP, Direction.DOWN]
+        case "|", Direction.DOWN:
+            return [Direction.DOWN]
+        case "|", Direction.LEFT:
+            return [Direction.UP, Direction.DOWN]
+        case "|", Direction.UP:
+            return [Direction.UP]
+
+
 def energize(grid: Grid, start_movement: BeamMovement) -> EnergizedGrid:
-
-
-    return []
+    energized_grid = [["." for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    q = MovementQueue()
+    q.put(start_movement)
+    next_movement = q.get()
+    while next_movement is not None:
+        _, (line, column) = next_movement
+        energized_grid[line][column] = "#"
+        more_movements = calculate_more_movements(grid, next_movement)
+        [q.put(mv) for mv in more_movements]
+        next_movement = q.get()
+    return energized_grid
 
 
 def count_energized_tiles(grid: EnergizedGrid) -> int:
-    return 0
+    return sum([sum([tile == "#" for tile in row]) for row in grid])
 
 
 def exercise_1(data: Grid) -> int:
--- 16/test_day_16.py
+++ 16/test_day_16.py
@@ -20,5 +20,62 @@ def test_movement_queue():
     assert result3 is None
 
 
+def test_new_direction():
+    # dot
+    assert calculate_new_directions(".", Direction.RIGHT) == [Direction.RIGHT]
+    assert calculate_new_directions(".", Direction.DOWN) == [Direction.DOWN]
+    assert calculate_new_directions(".", Direction.LEFT) == [Direction.LEFT]
+    assert calculate_new_directions(".", Direction.UP) == [Direction.UP]
+    # slash
+    assert calculate_new_directions("/", Direction.RIGHT) == [Direction.UP]
+    assert calculate_new_directions("/", Direction.DOWN) == [Direction.LEFT]
+    assert calculate_new_directions("/", Direction.LEFT) == [Direction.DOWN]
+    assert calculate_new_directions("/", Direction.UP) == [Direction.RIGHT]
+    # backslash
+    assert calculate_new_directions("\\", Direction.RIGHT) == [Direction.DOWN]
+    assert calculate_new_directions("\\", Direction.DOWN) == [Direction.RIGHT]
+    assert calculate_new_directions("\\", Direction.LEFT) == [Direction.UP]
+    assert calculate_new_directions("\\", Direction.UP) == [Direction.LEFT]
+    # dash
+    assert calculate_new_directions("-", Direction.RIGHT) == [Direction.RIGHT]
+    assert calculate_new_directions("-", Direction.DOWN) == [Direction.RIGHT, Direction.LEFT]
+    assert calculate_new_directions("-", Direction.LEFT) == [Direction.LEFT]
+    assert calculate_new_directions("-", Direction.UP) == [Direction.RIGHT, Direction.LEFT]
+    # pipe
+    assert calculate_new_directions("|", Direction.RIGHT) == [Direction.UP, Direction.DOWN]
+    assert calculate_new_directions("|", Direction.DOWN) == [Direction.DOWN]
+    assert calculate_new_directions("|", Direction.LEFT) == [Direction.UP, Direction.DOWN]
+    assert calculate_new_directions("|", Direction.UP) == [Direction.UP]
+
+def test_symbol_on_grid():
+    #dot
+    assert calculate_more_movements([[".", "."]], (Direction.RIGHT, (0, 0))) == [(Direction.RIGHT, (0, 1))]
+    assert calculate_more_movements([[".", "."]], (Direction.LEFT, (0, 1))) == [(Direction.LEFT, (0, 0))]
+    assert calculate_more_movements([["."], ["."]], (Direction.DOWN, (0, 0))) == [(Direction.DOWN, (1, 0))]
+    assert calculate_more_movements([["."], ["."]], (Direction.UP, (1, 0))) == [(Direction.UP, (0, 0))]
+
+    assert calculate_more_movements([["."]], (Direction.RIGHT, (0, 0))) == []
+    assert calculate_more_movements([["."]], (Direction.LEFT, (0, 0))) == []
+    assert calculate_more_movements([["."]], (Direction.DOWN, (0, 0))) == []
+    assert calculate_more_movements([["."]], (Direction.UP, (0, 0))) == []
+
+    #slash
+    assert calculate_more_movements([["."], ["/"]], (Direction.RIGHT, (1, 0))) == [(Direction.UP, (0, 0))]
+    assert calculate_more_movements([[".", "/"]], (Direction.DOWN, (0, 1))) == [(Direction.LEFT, (0, 0))]
+    assert calculate_more_movements([["/"], ["."]], (Direction.LEFT, (0, 0))) == [(Direction.DOWN, (1, 0))]
+    assert calculate_more_movements([["/", "."]], (Direction.UP, (0, 0))) == [(Direction.RIGHT, (0, 1))]
+
+    #backslash
+    assert calculate_more_movements([["\\"], ["."]], (Direction.RIGHT, (0, 0))) == [(Direction.DOWN, (1, 0))]
+
+    #dash
+    assert calculate_more_movements([["."], ["|"], ["."]], (Direction.RIGHT, (1, 0))) == [(Direction.UP, (0, 0)), (Direction.DOWN, (2, 0))]
+    assert calculate_more_movements([["|"], ["."]], (Direction.RIGHT, (0, 0))) == [(Direction.DOWN, (1, 0))]
+
+
 def test_exercise_1():
     assert exercise_1(data_small) == 46
+
+
+def test_exercise_1_full():
+    assert exercise_1(data_full) == 7951
