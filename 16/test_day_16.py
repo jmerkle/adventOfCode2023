d4460e277ed3886b5ca42dc8369ba98318829976
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-16 12:51:59 +0100

    day 16 exercise 1 read input as Grid

--- 16/test_day_16.py
+++ 16/test_day_16.py
@@ -1,7 +1,7 @@
 from functions_day_16 import *
 
-data_small = read_file_as_list_of_lines_and_filter_empty_lines('input_small.txt')
-data_full = read_file_as_list_of_lines_and_filter_empty_lines('input.txt')
+data_small = read_file_as_list_of_list_and_filter_empty_lines('input_small.txt')
+data_full = read_file_as_list_of_list_and_filter_empty_lines('input.txt')
 
 
 def test_exercise_1():
