21b7f8100d541eb1aca38fa18de12cc7e3ffef9b
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-26 21:10:02 +0100

    day 17 exercise 1 setup

---
+++ 17/functions_day_17.py
@@ -0,0 +1,15 @@
+from typing import TypeAlias
+
+
+Grid: TypeAlias = list[list[str]]
+
+
+def read_file_as_list_of_list_and_filter_empty_lines(filename: str) -> Grid:
+    f = open(filename, 'r')
+    data = f.read()
+    f.close()
+    return [list(x) for x in data.split("\n") if len(x) > 0]
+
+
+def exercise_1(data: Grid) -> int:
+    return 0
