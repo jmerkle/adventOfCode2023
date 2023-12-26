620f6972007d5cb849328ea843a2e7875ef3b31a
Johannes Merkle <johannesmerkle@gmx.de>
2023-12-26 21:54:09 +0100

    day 17 exercise 1 calculate possible movements

--- 17/functions_day_17.py
+++ 17/functions_day_17.py
@@ -1,14 +1,54 @@
+from enum import Enum
 from typing import TypeAlias
 
 
-Grid: TypeAlias = list[list[str]]
+class Direction(Enum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+
+MovementNode: TypeAlias = tuple[int, int, Direction, int]
+Grid: TypeAlias = list[list[int]]
 
 
 def read_file_as_list_of_list_and_filter_empty_lines(filename: str) -> Grid:
     f = open(filename, 'r')
     data = f.read()
     f.close()
-    return [list(x) for x in data.split("\n") if len(x) > 0]
+    return [[int(entry) for entry in list(line)] for line in data.split("\n") if len(line) > 0]
+
+
+def calculate_possible_movements(data: Grid, position: MovementNode) -> dict[MovementNode, int]:
+    row, column, direction, steps_taken = position
+    possible_movements = {}
+    # up
+    if row > 0:
+        if direction is not Direction.UP:
+            possible_movements.update({(row-1, column, Direction.UP, 1): data[row-1][column]})
+        elif steps_taken < 3:
+            possible_movements.update({(row-1, column, Direction.UP, steps_taken + 1): data[row-1][column]})
+    # right
+    if column < len(data[0])-1:
+        if direction is not Direction.RIGHT:
+            possible_movements.update({(row, column+1, Direction.RIGHT, 1): data[row][column+1]})
+        elif steps_taken < 3:
+            possible_movements.update({(row, column+1, Direction.RIGHT, steps_taken + 1): data[row][column+1]})
+    # down
+    if row < len(data)-1:
+        if direction is not Direction.DOWN:
+            possible_movements.update({(row+1, column, Direction.DOWN, 1): data[row+1][column]})
+        elif steps_taken < 3:
+            possible_movements.update({(row+1, column, Direction.DOWN, steps_taken + 1): data[row+1][column]})
+    # left
+    if column > 0:
+        if direction is not Direction.LEFT:
+            possible_movements.update({(row, column-1, Direction.LEFT, 1): data[row][column-1]})
+        elif steps_taken < 3:
+            possible_movements.update({(row, column-1, Direction.LEFT, steps_taken + 1): data[row][column-1]})
+    return possible_movements
+
 
 
 def exercise_1(data: Grid) -> int:
--- 17/test_day_17.py
+++ 17/test_day_17.py
@@ -4,5 +4,33 @@ data_small = read_file_as_list_of_list_and_filter_empty_lines('input_small.txt')
 data_full = read_file_as_list_of_list_and_filter_empty_lines('input.txt')
 
 
+def test_possible_movements():
+    grid = [[2, 4, 1],
+            [3, 2, 1],
+            [3, 2, 5]]
+
+    assert (calculate_possible_movements(grid, (0, 0, Direction.UP, 0)) ==
+            {
+                (0, 1, Direction.RIGHT, 1): 4,
+                (1, 0, Direction.DOWN, 1): 3
+            })
+
+    assert (calculate_possible_movements(grid, (1, 1, Direction.UP, 2)) ==
+            {
+                (0, 1, Direction.UP, 3): 4,
+                (1, 2, Direction.RIGHT, 1): 1,
+                (2, 1, Direction.DOWN, 1): 2,
+                (1, 0, Direction.LEFT, 1): 3
+            })
+
+    assert (calculate_possible_movements(grid, (1, 1, Direction.UP, 3)) ==
+            {
+                (1, 2, Direction.RIGHT, 1): 1,
+                (2, 1, Direction.DOWN, 1): 2,
+                (1, 0, Direction.LEFT, 1): 3
+            })
+
+
+
 def test_exercise_1():
     assert exercise_1(data_small) == 102
