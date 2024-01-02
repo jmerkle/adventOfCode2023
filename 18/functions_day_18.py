b09f47799a0519e3877c2ef4d1c41210d72a993d
Johannes Merkle <johannesmerkle@gmx.de>
2024-01-02 16:31:29 +0100

    day 18 exercise 1 resize grid

--- 18/functions_day_18.py
+++ 18/functions_day_18.py
@@ -18,11 +18,28 @@ class Direction(Enum):
 
 Command: TypeAlias = tuple[Direction, int, hex]
 Grid: TypeAlias = list[list[str]]
+Position: TypeAlias = tuple[int, int]
 
 
 def parse_command(command_string: str) -> Command:
     d, n, h = command_string.split()
     return Direction(d), int(n), int("0x" + h[2:-1], base=16)
+
+
+def draw(grid: Grid, position: Position, command: Command) -> tuple[Grid, Position]:
+    return 0
+
+
+def resize_right(grid: Grid, n: int) -> Grid:
+    [row.extend(["." for _ in range(n)]) for row in grid]
+    return grid
+
+
+def resize_down(grid: Grid, n: int) -> Grid:
+    num_columns = len(grid[0])
+    new_rows = [["." for _ in range(num_columns)] for _ in range(n)]
+    grid = grid.extend(new_rows)
+    return grid
 
 
 def exercise_1(data: list[str]) -> int:
--- 18/test_day_18.py
+++ 18/test_day_18.py
@@ -8,5 +8,58 @@ def test_parse_command():
     assert parse_command("R 6 (#70c710)") == (Direction.RIGHT, 6, 0X70c710)
 
 
+def test_draw_command():
+    command = (Direction.RIGHT, 6, 0X70c710)
+    grid = [["."]]
+    position = (0, 0)
+    assert draw(grid, position, command) == ([["#", "#", "#", "#", "#", "#"]], (0, 5))
+
+
+def test_resize_new_grid_right():
+    grid = [["."]]
+    resize_right(grid, 5)
+    assert grid == [[".", ".", ".", ".", ".", "."]]
+
+
+def test_resize_existing_grid_right():
+    grid = [
+        ["#", "#", "#"],
+        ["#", ".", "#"],
+        ["#", ".", "#"],
+        ["#", "#", "#"]
+            ]
+    resize_right(grid, 2)
+    assert grid == [
+        ["#", "#", "#", ".", "."],
+        ["#", ".", "#", ".", "."],
+        ["#", ".", "#", ".", "."],
+        ["#", "#", "#", ".", "."]
+    ]
+
+
+def test_resize_new_grid_down():
+    grid = [["."]]
+    resize_down(grid, 5)
+    assert grid == [["."], ["."], ["."], ["."], ["."], ["."]]
+
+
+def test_resize_existing_grid_down():
+    grid = [
+        ["#", "#", "#"],
+        ["#", ".", "#"],
+        ["#", ".", "#"],
+        ["#", "#", "#"]
+    ]
+    resize_down(grid, 2)
+    assert grid == [
+        ["#", "#", "#"],
+        ["#", ".", "#"],
+        ["#", ".", "#"],
+        ["#", "#", "#"],
+        [".", ".", "."],
+        [".", ".", "."]
+    ]
+
+
 def test_exercise_1():
     assert exercise_1(data_small) == 62
